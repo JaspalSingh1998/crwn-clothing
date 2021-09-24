28bb77e051766071b1fc266dd4a58d504032e3f6
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-24 14:16:59 +0530

    Homepage and sass setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+
+import HomePage from "./homepage.component";
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <HomePage />
     </div>
   );
 }
