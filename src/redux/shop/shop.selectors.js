e6de793bd0958f81cfeb11a3d22e454d082489b7
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-30 13:48:21 +0530

    Data normalization

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import { createSelector } from "reselect";
 import memoize from "lodash.memoize";
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 
 const selectShop = (state) => state.shop;
 
@@ -16,9 +9,8 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   )
 );
