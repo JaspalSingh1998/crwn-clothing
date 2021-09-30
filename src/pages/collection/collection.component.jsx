2f4e292fb183708f7d02134ee1387a3f393b10d3
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-30 12:44:23 +0530

    Added Collection Page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import "./collection.styles.scss";
+
+import collectionItemComponent from "../../components/collection-item/collection-item.component";
+
+const CollectionPage = () => (
+  <div className="category">
+    <h2>Collection Page</h2>
+  </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,12 @@
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
